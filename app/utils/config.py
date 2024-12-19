fd5dded97b408be463303b15cd66bde76c465a78
Nekender Shekhawat <108869998+nekender@users.noreply.github.com>
2024-12-19 17:07:32 +0530

    Local DB changes

--- app/utils/config.py
+++ app/utils/config.py
@@ -27,10 +27,10 @@ class Settings(BaseSettings):
     STAGE: str= "PROD"
 
 
+    DB_SSLMODE: str = "disable"
     @property
     def DATABASE_URL(self):
-        url = f"postgresql://{self.DB_USER}:{quote_plus(self.DB_PASSWORD)}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}?sslmode=require"
-        print(f"Constructed DATABASE_URL: {url}")
+        url = f"postgresql://{self.DB_USER}:{quote_plus(self.DB_PASSWORD)}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}?sslmode={self.DB_SSLMODE}"
         return url
 
     @property
